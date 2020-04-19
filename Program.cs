666e8524b2a4ccd8f7546925b5bef7f723397370
Dieva Leon <58702512+dievaGit@users.noreply.github.com>
2020-04-19 15:07:30 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Exercise_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring variables
+            uint n = 0, count = 0;
+            BigInteger f = 1, module = 0;
+
+            //Requesting number
+            Console.Write(" Enter the positive number: ");
+            n = Convert.ToUInt32(Console.ReadLine());
+
+            //factorial
+            for (uint i = n; i > 0; i--)
+            {
+                f *= i;
+            }
+
+            //Looking for zeros at the end of the factorial
+            module = f;
+            while((module %10) == 0)
+            {
+                count++;
+                module /= 10;
+            }
+
+            Console.Write(" N = {0} -> N! = {1} -> Have {2} Zeros.", n, f, count);
+
+            Console.ReadKey();
+        }
+    }
+}
